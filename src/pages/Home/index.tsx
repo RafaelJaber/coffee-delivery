6d46b86cc98b65a81791145ee36cb580c482e54d
Rafael Jáber <rafael.jaber@hotmail.com>
2023-05-31 01:55:00 -0300

    implementada a section de informativos sobre os cafés

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,58 @@
+import { Coffee, Package, ShoppingCart, Timer } from '@phosphor-icons/react'
+
+import coffeeDelivery from '../../assets/coffeDelivery.svg'
+import {
+  BannerLayout,
+  ContentSectionDivLayout,
+  ItemIconSectionLayout,
+  ItensContentSectionLayout,
+} from './styled.ts'
+import { defaultTheme } from '../../styles/themes/default.ts'
+
 export function Home() {
-  return <h1>Home</h1>
+  return (
+    <BannerLayout>
+      <ContentSectionDivLayout>
+        <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+        <p>
+          Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+          hora
+        </p>
+        <ItensContentSectionLayout>
+          <div>
+            <p>
+              <ItemIconSectionLayout bgColor={defaultTheme['yellow-700']}>
+                <ShoppingCart size={16} weight={'fill'} />
+              </ItemIconSectionLayout>
+              Compra simples e segura
+            </p>
+            <p>
+              <ItemIconSectionLayout bgColor={defaultTheme['gray-800']}>
+                <Package size={16} weight="fill" />
+              </ItemIconSectionLayout>
+              Embalagem mantém o café intacto
+            </p>
+          </div>
+          <div>
+            <p>
+              <ItemIconSectionLayout bgColor={defaultTheme['yellow-500']}>
+                <Timer size={16} weight="fill" />
+              </ItemIconSectionLayout>
+              Entrega rápida e rastreada
+            </p>
+            <p>
+              <ItemIconSectionLayout bgColor={defaultTheme['purple-700']}>
+                <Coffee size={16} weight="fill" />
+              </ItemIconSectionLayout>
+              O café chega fresquinho até você
+            </p>
+          </div>
+        </ItensContentSectionLayout>
+      </ContentSectionDivLayout>
+      <img
+        src={coffeeDelivery}
+        alt="Copo de café com fundo amarelo e gãos de café ao lado"
+      />
+    </BannerLayout>
+  )
 }
---
+++ src/pages/Home/styled.ts
@@ -0,0 +1,68 @@
+import styled from 'styled-components'
+
+import backgroundSection from '../../assets/Background.svg'
+import { defaultTheme } from '../../styles/themes/default.ts'
+
+interface ButtonIconsProps {
+  bgColor: typeof defaultTheme | string
+}
+
+export const BannerLayout = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 5.5rem 10rem;
+  background-image: url(${backgroundSection});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`
+
+export const ContentSectionDivLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  h1 {
+    font-weight: 800;
+    font-family: 'baloo 2', sans-serif;
+    font-size: 3rem;
+    color: ${(props) => props.theme['gray-900']};
+    line-height: 1.4;
+  }
+
+  p {
+    font-size: 1.25rem;
+    line-height: 1.2;
+  }
+`
+
+export const ItensContentSectionLayout = styled.div`
+  margin-top: 1rem;
+
+  div {
+    display: flex;
+    gap: 40px;
+    margin-bottom: 1.594rem;
+  }
+
+  p {
+    font-size: 1rem;
+    display: flex;
+    align-items: center;
+    gap: 0.75rem;
+    width: 18.75rem;
+  }
+`
+
+export const ItemIconSectionLayout = styled.span<ButtonIconsProps>`
+  background: ${(props) => props.bgColor};
+  color: ${(props) => props.theme.white};
+  border-radius: 50%;
+  display: flex;
+  width: 2rem;
+  height: 2rem;
+  justify-content: center;
+  align-items: center;
+`
